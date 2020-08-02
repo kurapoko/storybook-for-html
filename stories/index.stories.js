47009cc1054e9d8df84ba2b41bf1af9c3d7fefd4
m.kurahara <kuraharamasahiro@gmail.com>
2020-08-03 08:36:04 +0900

    knob追加

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,13 +1,23 @@
-export default {
-  title: 'Demo',
-};
+// export default {
+//   title: 'Demo',
+// };
 
-export const Heading = () => '<h1>Hello World</h1>';
+// export const Heading = () => '<h1>Hello World</h1>';
 
-export const Button = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Hello Button';
-  btn.addEventListener('click', e => console.log(e));
-  return btn;
+// export const Button = () => {
+//   const btn = document.createElement('button');
+//   btn.type = 'button';
+//   btn.innerText = 'Hello Button';
+//   btn.addEventListener('click', e => console.log(e));
+//   return btn;
+// };
+
+export default { title: 'Button' };
+
+export const withText = () => '<button class="btn">Hello World</button>';
+
+export const withEmoji = () => {
+  const button = document.createElement('button');
+  button.innerText = '😀 😎 👍 💯';
+  return button;
 };
